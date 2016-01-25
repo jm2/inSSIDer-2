2b782f0d68ffc5a600f278164defbd501faa9b4b
Ste Griffiths <github@stegriff.co.uk>
2016-01-25 20:48:51 +0000

    Flattened submodules into local code

---
+++ IoctlNdis/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MetaGeek.IoctlNdis")]
+[assembly: AssemblyDescription("Ndis query API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MetaGeek, LLC")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © MetaGeek, LLC 2005-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26cad5e8-3b89-45fd-a45b-2f034b58f729")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyFileVersion("1.1.1.1366")]
+[assembly: AssemblyVersion("1.1.1.1366")]
